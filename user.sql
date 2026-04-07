d9ded7cb955dcaf152e04724c9fc6299454f6e2a
Anas <Anas.Touil@esprit.tn>
2026-04-07 22:50:12 +0100

    User DataBase

---
+++ user.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `user` (
+    `id_user`            INT            NOT NULL AUTO_INCREMENT,
+    `nom_complet`        VARCHAR(100)   NOT NULL,
+    `nom_user`           VARCHAR(50)    NOT NULL,
+    `mot_de_passe`       VARCHAR(255)   NOT NULL,
+    `age`                INT            DEFAULT NULL,
+    `poids`              FLOAT          DEFAULT NULL,
+    `taille`             INT            DEFAULT NULL,
+    `email`              VARCHAR(100)   NOT NULL,
+    `pays`               VARCHAR(50)    DEFAULT NULL,
+    `ville`              VARCHAR(50)    DEFAULT NULL,
+    `tel`                VARCHAR(20)    DEFAULT NULL,
+    `occupation`         VARCHAR(100)   DEFAULT NULL,
+    `date_de_naissance`  DATE           DEFAULT NULL,
+    PRIMARY KEY (`id_user`),
+    UNIQUE KEY `uk_nom_user` (`nom_user`)
+);
