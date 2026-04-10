2e676fc1e8844650e879066e183b370ef9c278c3
roua kadri <rouakadri55@gmail.com>
2026-04-10 16:33:24 -0700

    Add files via upload
    tab_stand_produit_final

---
+++ barchathon (1).sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : sam. 11 avr. 2026 à 01:31
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `barchathon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `ID_produit` int(11) NOT NULL,
+  `ID_stand` int(11) DEFAULT NULL,
+  `nom_produit` varchar(100) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `prix_produit` decimal(10,2) DEFAULT NULL,
+  `qte_stock` int(11) DEFAULT NULL,
+  `en_out_stock` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stand`
+--
+
+CREATE TABLE `stand` (
+  `ID_stand` int(11) NOT NULL,
+  `nom_stand` varchar(100) DEFAULT NULL,
+  `position` varchar(100) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `ID_parcours` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`ID_produit`),
+  ADD KEY `ID_stand` (`ID_stand`);
+
+--
+-- Index pour la table `stand`
+--
+ALTER TABLE `stand`
+  ADD PRIMARY KEY (`ID_stand`),
+  ADD KEY `ID_parcours` (`ID_parcours`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `ID_produit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `stand`
+--
+ALTER TABLE `stand`
+  MODIFY `ID_stand` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`ID_stand`) REFERENCES `stand` (`ID_stand`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
